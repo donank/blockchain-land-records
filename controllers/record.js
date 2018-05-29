d0e3f86be047454c71562f3b5912008103158a8f
donank <anandankit@protonmail.com>
2018-05-29 10:34:15 +0530

    basic record model

---
+++ controllers/record.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var ipfsAPI = require('ipfs-api')
+
+var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'})
+
+module.exports = (req, res) => {
+    
+}
--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const Express = require('express');
-const app = new Express();
+const router = new Express();
 
-const Web3 = require('web3');
-const web3 = new Web3();
-
-const IPFS = require('ipfs')
-const node = new IPFS()
