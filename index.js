1785016c101394c2e8c99d0f67851494fbb1d09f
donank <anandankit@protonmail.com>
2018-06-15 20:45:55 +0530

    Fixed Missing semi-colon

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ var server = app.listen( process.env.PORT || 3000, function(){
     console.log('Listening on port ' + server.address().port);
   });
 
-module.exports = server
+module.exports = server;
